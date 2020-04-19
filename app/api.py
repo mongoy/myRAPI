f8c0fd4869dfb3c00aa86926dca0ad1eeaab9fc1
Kibotenok <nikitaantropov@inbox.ru>
2020-04-20 01:47:11 +0900

    begin api build. POST

--- app/api.py
+++ app/api.py
@@ -1,12 +1,14 @@
+import json
 import redis
 import settings
 from flask_api import FlaskAPI, status
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, Response
+
 # from flask_restful import Api, Resource
 
 app = FlaskAPI(__name__)
 # Connect to our Redis instance
-redis_instance = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+#redis_instance = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
 
 
 # easy web
@@ -22,29 +24,33 @@ def Flask_API():
     return {'hello': 'FLASK API'}
 
 
-# RASTfullAPI
-# @app.route('/', methods=['GET', 'POST'])
-# def visit_list():
-#     """
-#         List urls
-#     """
-#     if request.method == 'POST':
-#         return {'method': 'POST'}
-#     else:
-#         return {'method': 'GET'}
-
 @app.route('/api/visited_domains', methods=['GET'])
 def visited_domains():
     """
         Print urls visited
     """
-    # if 'name' in request.args:
-    #     return 'Hello ' + request.args['name']
-    #
-    # else:
-    #     return 'Hello John Doe'
-    return 'Hello '# , status.HTTP_200_OK
-    # return {'method': 'GET'}
+    links = redis.Redis()
+    items = []
+    count = 0
+    for key in links.keys("*"):
+        items.append(links.get(key).decode("utf-8"))
+        # items.append(str(links.get(key)))
+        count += 1
+    res_links = ','.join(items)
+    if len(items) == 0:
+        response = {
+            'count': 0,
+            'msg': "Found 0 items.",
+            'items': "-"
+        }
+    else:
+        response = {
+            'count': count,
+            'links': f"Found {count} links.",
+            'items': res_links
+        }
+    return response  #, 200, {'Content-Type': 'application/json'}  #Response(response, status=200)
+    #return {'method': 'GET'}
 
 
 @app.route('/api/visited_links', methods=['POST'])
