354fbf5b9e03a537df3cae956b691f678a2d45da
Kibotenok <nikitaantropov@inbox.ru>
2020-04-20 23:56:50 +0900

    api build. POST

--- app/api.py
+++ app/api.py
@@ -1,14 +1,15 @@
 import json
 import redis
 import settings
+from datetime import datetime
 from flask_api import FlaskAPI, status
-from flask import Flask, url_for, request, Response
+from flask import Flask, url_for, request, Response, jsonify
 
 # from flask_restful import Api, Resource
 
 app = FlaskAPI(__name__)
 # Connect to our Redis instance
-#redis_instance = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+# redis_instance = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
 
 
 # easy web
@@ -31,26 +32,22 @@ def visited_domains():
     """
     links = redis.Redis()
     items = []
-    count = 0
     for key in links.keys("*"):
         items.append(links.get(key).decode("utf-8"))
-        # items.append(str(links.get(key)))
-        count += 1
+
     res_links = ','.join(items)
+
     if len(items) == 0:
         response = {
-            'count': 0,
-            'msg': "Found 0 items.",
-            'items': "-"
+            "domains": ""
         }
     else:
         response = {
-            'count': count,
-            'links': f"Found {count} links.",
-            'items': res_links
+            "domains": items
         }
-    return response  #, 200, {'Content-Type': 'application/json'}  #Response(response, status=200)
-    #return {'method': 'GET'}
+    return jsonify(response)
+    # , 200, {'Content-Type': 'application/json'}  #Response(response, status=200)
+    # return {'method': 'GET'}
 
 
 @app.route('/api/visited_links', methods=['POST'])
@@ -58,7 +55,11 @@ def visited_links():
     """
         Add urls visited
     """
+
     if request.method == 'POST':
+        # временная метка
+        timestamp = round(datetime.today().timestamp())
+
         return {'method': 'POST'}
 
 
